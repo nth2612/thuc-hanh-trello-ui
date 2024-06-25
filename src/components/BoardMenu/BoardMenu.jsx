af6fdff93531e16321825e1f62f12534ea64a8a6
nth2612 <nguyentuanhaivjppro100@gmail.com>
2024-06-25 11:16:38 +0700

    what a day

--- src/components/BoardMenu/BoardMenu.jsx
+++ src/components/BoardMenu/BoardMenu.jsx
@@ -5,7 +5,7 @@ import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
 import ListIcon from '@mui/icons-material/List'
 import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined'
 import SettingsIcon from '@mui/icons-material/Settings'
-import SquareRoundedIcon from '@mui/icons-material/SquareRounded';
+import SquareRoundedIcon from '@mui/icons-material/SquareRounded'
 import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh'
 import PhotoFilterIcon from '@mui/icons-material/PhotoFilter'
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch'
@@ -20,77 +20,83 @@ import RemoveIcon from '@mui/icons-material/Remove'
 const listMenuItem = [
   {
     id: 1,
-    icon: <InfoOutlinedIcon/>,
+    icon: <InfoOutlinedIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'About this board'
   },
   {
     id: 2,
-    icon: <ListIcon/>,
+    icon: <ListIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Activity'
   },
   {
     id: 3,
-    icon: <ArchiveOutlinedIcon/>,
+    icon: <ArchiveOutlinedIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Archived items'
   },
   {
+    id: ''
+  },
+  {
     id: 4,
-    icon: <SettingsIcon/>,
+    icon: <SettingsIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Settings'
   },
   {
     id: 5,
-    icon: <SquareRoundedIcon/>,
+    icon: <SquareRoundedIcon sx={{ color: '#0079bf' }} fontSize='small' />,
     textMenu: 'Change background'
   },
   {
     id: 6,
-    icon: <AutoFixHighIcon/>,
+    icon: <AutoFixHighIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Custom Fields'
   },
   {
     id: 7,
-    icon: <PhotoFilterIcon/>,
+    icon: <PhotoFilterIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Automation'
   },
   {
     id: 8,
-    icon: <RocketLaunchIcon/>,
+    icon: <RocketLaunchIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Power-Ups'
   },
   {
     id: 9,
-    icon: <BookmarkBorderIcon/>,
+    icon: <BookmarkBorderIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Labels'
   },
   {
     id: 10,
-    icon: <EmojiEmotionsOutlinedIcon/>,
+    icon: <EmojiEmotionsOutlinedIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Stickers'
   },
   {
+    id: ''
+  },
+  {
     id: 11,
-    icon: <RemoveRedEyeOutlinedIcon/>,
+    icon: <RemoveRedEyeOutlinedIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Watch'
   },
   {
     id: 12,
-    icon: <ContentCopyIcon/>,
+    icon: <ContentCopyIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Copy board'
   },
   {
     id: 13,
-    icon : <MailOutlineRoundedIcon/>,
+    icon : <MailOutlineRoundedIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Email-to-board'
   },
   {
     id: 14,
-    icon: <ShareOutlinedIcon/>,
+    icon: <ShareOutlinedIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Print, export and share'
   },
   {
     id: 15,
-    icon: <RemoveIcon/>,
+    icon: <RemoveIcon sx={{ color: '#44546f' }} fontSize='small' />,
     textMenu: 'Close board'
   }
 ]
@@ -110,11 +116,21 @@ function BoardMenu({ handleOpen, open }) {
             </IconButton>
           </ListItem>
           <Divider variant='middle' />
-          {listMenuItem.map(item => (
-            <ListItemButton sx={{ padding: '0 12px' }} key={item.id}>
-              {item.icon}
-            </ListItemButton>
-          ))}
+          <Box sx={{ padding: '12px 12px 8px' }} >
+            {listMenuItem.map(item => {
+              if (item.id) {
+                return (
+                  <ListItemButton sx={{ padding: '6px', borderRadius: '8px', mb: '4px' }} key={item.id}>
+                    {item.icon}
+                    <Typography sx={{ color: '#172b4d', ml: '12px' }} >{item.textMenu}</Typography>
+                  </ListItemButton>
+                )
+              }
+              return (
+                <Divider sx={{ my: '8px' }} key={Math.random()} />
+              )
+            })}
+          </Box>
         </List>
       </Box>
     </>
