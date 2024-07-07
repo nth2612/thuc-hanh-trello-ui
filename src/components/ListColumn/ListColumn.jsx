2f47a16a80392719409c39f36745b3cff64255d7
nth2612 <nguyentuanhaivjppro100@gmail.com>
2024-07-07 22:09:46 +0700

    last day

--- src/components/ListColumn/ListColumn.jsx
+++ src/components/ListColumn/ListColumn.jsx
@@ -1,13 +1,19 @@
-import { Box, IconButton } from '@mui/material'
+import { Box, Button, IconButton } from '@mui/material'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
 import { useEffect, useRef, useState } from 'react'
+import CopyAllIcon from '@mui/icons-material/CopyAll'
+import AddIcon from '@mui/icons-material/Add'
 
 function ListColumn() {
   const textareaRef = useRef(null)
-  const h2Ref = useRef(null)
+  const h2Ref = useRef(0)
   const [editText, setEditText] = useState(false)
-  const [initText, setInitText] = useState('Ngọt band number one, abncam,scnnas,mcnkwhkcabscjkabsckjabsjkcbjkasbckbansc,nákjcn')
+  const [initText, setInitText] = useState('Mất tích trong đám đông giao thừa, pháo hoa cháy trong mưa, soi hết đoàn người')
   const [h2Height, setH2Height] = useState(0)
+  const handleClickH2 = () => {
+    setEditText(true)
+    setH2Height(h2Ref.current.offsetHeight)
+  }
   const handleChangeEditText = (e) => {
     setInitText(e.target.value)
   }
@@ -25,16 +31,10 @@ function ListColumn() {
       textareaRef.current.select()
     }
   }, [editText])
-  useEffect(() => {
-    if (h2Ref.current) {
-      console.log(h2Ref.current.offsetHeight);
-      setH2Height(h2Ref.current.offsetHeight)
-    }
-  }, [])
   return (
     <Box sx={{ mt: '12px', flexGrow: 1 }} >
       <Box sx={{ height: '100%' }} >
-        <Box sx={{ padding: '2px 6px 8px', height: '100%' }} >
+        <Box sx={{ padding: '2px 6px 8px', height: '100%', display: 'flex', flexDirection: 'row' }} >
           <Box sx={{ paddingX: '6px', flexShrink: 0, height: '100%' }}>
             <Box sx={{
               display: 'flex',
@@ -47,11 +47,53 @@ function ListColumn() {
             }}>
               <Box sx={{ padding: '8px 8px 0', display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }} >
                 <Box sx={{ flex: 1 }} >
-                  <h2 ref={h2Ref} onClick={() => setEditText(true)} style={{ display: editText ? 'none' : 'block', color: '#172b4d', fontSize: '14px', padding: '6px 8px 6px 12px', cursor: 'pointer', fontWeight: '500', lineHeight: '20px', overflowWrap: 'anywhere' }}>{initText}</h2>
-                  {editText && <textarea ref={textareaRef} onChange={handleChangeEditText} onKeyDown={handleKeyDown} name='' id='' autoFocus onBlur={handleBlur} value={initText} ></textarea>}
+                  <h2 ref={h2Ref} onClick={handleClickH2} style={{ display: editText ? 'none' : 'block', letterSpacing: 'normal', color: '#172b4d', fontSize: '14px', padding: '6px 8px 6px 12px', cursor: 'pointer', fontWeight: '500', lineHeight: '20px', overflowWrap: 'anywhere' }}>{initText}</h2>
+                  <textarea
+                    style={{ height: `${h2Height}px`, display: editText ? 'block' : 'none' }}
+                    ref={textareaRef}
+                    onChange={handleChangeEditText}
+                    onKeyDown={handleKeyDown}
+                    name='' id=''
+                    autoFocus
+                    onBlur={handleBlur}
+                    value={initText} >
+                  </textarea>
                 </Box>
                 <IconButton sx={{ borderRadius: '8px', flexShrink: 0, padding: '6px', '&:hover' : { bgcolor: '#091e4224' }, '&:hover .MuiSvgIcon-root' : { color: '#44546f' } }} >
                   <MoreHorizIcon sx={{ color: '#626f86' }} fontSize='small' />
+                </IconButton>
+              </Box>
+              <Box sx={{ height: '8px', mb: '-2px' }} ></Box>
+              <Box sx={{ p: '2px 4px', mx: '4px', overflowX: 'hidden', overflowY: 'auto', flexDirection: 'column', rowGap: '8px', display: 'flex', flex: '1 1 auto' }} >
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+                <Box>Xin chào</Box>
+              </Box>
+              <Box sx={{ padding: '8px 8px 0', display: 'flex', alignItems: 'center', justifyContent: 'space-between', columnGap: '4px' }} >
+                <Button startIcon={<AddIcon/>} sx={{ color: '#44546f', justifyContent: 'flex-start', lineHeight: 1, '&:hover' : { bgcolor: '#091e4224', color: '#172b4d' } }} fullWidth >Add a card</Button>
+                <IconButton sx={{ borderRadius: '8px', '&:hover' : { bgcolor: '#091e4224', color: '#172b4d' } }} >
+                  <CopyAllIcon sx={{ fontSize: '16px' }} />
                 </IconButton>
               </Box>
             </Box>
