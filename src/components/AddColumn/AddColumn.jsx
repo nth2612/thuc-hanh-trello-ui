16bfa33cba5f79555ca0cb17b6d8db6d051ce6ed
nth2612 <nguyentuanhaivjppro100@gmail.com>
2024-07-11 16:44:27 +0700

    well done

--- src/components/AddColumn/AddColumn.jsx
+++ src/components/AddColumn/AddColumn.jsx
@@ -1,25 +1,77 @@
 import AddIcon from '@mui/icons-material/Add'
 import { Box, Button, IconButton } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { mockData } from '~/apis/mock-data'
 
 function AddColumn() {
+  const textareaRef = useRef(null)
+  const formRef = useRef(null)
   const [openInput, setOpenInput] = useState(false)
+  const [nameColumn, setNameColumn] = useState('')
+  const [newData, setNewData] = useState(mockData.board.columns)
+  useEffect(() => {
+    const handleBlurForm = (event) => {
+      if (formRef.current) {
+        const clickedOutside = formRef.current.contains(event.target)
+        if (!clickedOutside) {
+          setOpenInput(false)
+        }
+      }
+    }
+    if (openInput) {
+      document.addEventListener('click', handleBlurForm)
+    }
+    return () => {
+      if (openInput) {
+        document.removeEventListener('click', handleBlurForm)
+      }
+    }
+  }, [openInput])
+  const handleClickTextarea = (event) => {
+    event.stopPropagation()
+    textareaRef.current.focus()
+  }
+  const blurTextarea = () => {
+    textareaRef.current.blur()
+  }
+  const hideTextarea = (event) => {
+    event.stopPropagation()
+    setOpenInput(false)
+  }
+  const addNewColumn = (event) => {
+    event.stopPropagation()
+    if (nameColumn !== '') {
+      console.log(nameColumn);
+    } else {
+      console.log('nhap vao di ma')
+    }
+  }
+  const handleChange = (input) => {
+    setNameColumn(input.target.value)
+  }
+  const openAddColumn = (event) => {
+    event.stopPropagation()
+    setOpenInput(true)
+  }
   return (
-    <Box sx={{ paddingX: '6px', flexShrink: 0, height: '100%' }}>
+    <Box sx={{ paddingX: '6px', flexShrink: 0, height: '100%' }} >
       {openInput ?
-        <form style={{ width: '272px', borderRadius: '12px', padding: '8px', backgroundColor: '#f1f2f4' }} >
-          <textarea autoFocus placeholder='Enter list title..' style={{ height: '32px', borderRadius: '4px' }} onBlur={() => setOpenInput(false)} ></textarea>
+        <form ref={formRef} onClick={blurTextarea} style={{ width: '272px', borderRadius: '12px', padding: '8px', backgroundColor: '#f1f2f4' }} >
+          <textarea value={nameColumn} ref={textareaRef} autoFocus placeholder='Enter list title..' style={{ height: '32px', borderRadius: '4px' }}
+            onClick={handleClickTextarea}
+            onChange={handleChange}
+          ></textarea>
           <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', gap: '4px', mt: '8px' }} >
-            <Button onClick={() => console.log('hihihi')} sx={{ bgcolor: '#0c66e4', color: '#fff', fontSize: '14px', p: '6px 12px', lineHeight: '20px', '&:hover' : { bgcolor: '#0055cc' } }} >Add list</Button>
-            <IconButton sx={{ padding: '6px', borderRadius: '3px', color: '#172b4d', '&:hover' : { bgcolor: '#091e4224' } }} >
+            <Button onClick={addNewColumn} sx={{ bgcolor: '#0c66e4', color: '#fff', fontSize: '14px', p: '6px 12px', lineHeight: '20px', '&:hover' : { bgcolor: '#0055cc' } }} >Add list</Button>
+            <IconButton onClick={hideTextarea} sx={{ padding: '6px', borderRadius: '3px', color: '#172b4d', '&:hover' : { bgcolor: '#091e4224' } }} >
               <CloseIcon fontSize='small' />
             </IconButton>
           </Box>
         </form>
         : <Button
           startIcon={<AddIcon />}
-          onClick={() => setOpenInput(true)}
+          onClick={openAddColumn}
           sx={{
             width: '272px',
             justifyContent: 'flex-start',
