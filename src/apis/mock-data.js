d715d407e126839149d9be57ea39dcecacffb22a
nth2612 <nguyentuanhaivjppro100@gmail.com>
2024-07-16 16:41:37 +0700

    thay doi mock data

--- src/apis/mock-data.js
+++ src/apis/mock-data.js
@@ -36,7 +36,7 @@ export const mockData = {
         _id: 'column-id-03',
         boardId: 'board-id-02',
         title: 'Gieo',
-        cardOrderIds: ['card-id-01', 'card-id-02', 'card-id-03', 'card-id-04', 'card-id-05', 'card-id-06', 'card-id-07', 'card-id-08', 'card-id-09', 'card-id-10', 'card-id-11', 'card-id-12', 'card-id-13', 'card-id-14', 'card-id-15', 'card-id-16', 'card-id-17', 'card-id-18', 'card-id-19'],
+        cardOrderIds: ['card-id-01', 'card-id-02', 'card-id-03', 'card-id-04', 'card-id-05', 'card-id-06', 'card-id-07', 'card-id-08', 'card-id-09', 'card-id-10', 'card-id-11'],
         card: [
           { _id: 'card-id-01', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Bạn thỏ tivi nhỏ', memberIds: [] },
           { _id: 'card-id-02', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Mấy khi', memberIds: [] },
@@ -48,15 +48,7 @@ export const mockData = {
           { _id: 'card-id-08', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Đá tan', memberIds: [] },
           { _id: 'card-id-09', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Đêm hôm qua', memberIds: [] },
           { _id: 'card-id-10', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Gieo', memberIds: [] },
-          { _id: 'card-id-11', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Mất tích', memberIds: [] },
-          { _id: 'card-id-12', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Nứt', memberIds: [] },
-          { _id: 'card-id-13', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Lần cuối', memberIds: [] },
-          { _id: 'card-id-14', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Màu (đen trắng)', memberIds: [] },
-          { _id: 'card-id-15', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Tôi (đi trú đông)', memberIds: [] },
-          { _id: 'card-id-16', boardId: 'board-id-02', columnId: 'column-id-03', title: '(bé)', memberIds: [] },
-          { _id: 'card-id-17', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Chuyển kênh', memberIds: [] },
-          { _id: 'card-id-18', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Hết thời', memberIds: [] },
-          { _id: 'card-id-19', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Em có chắc không', memberIds: [] }
+          { _id: 'card-id-11', boardId: 'board-id-02', columnId: 'column-id-03', title: 'Mất tích', memberIds: [] }
         ]
       },
       {
